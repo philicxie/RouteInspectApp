ba41c9b455f5e659cb2524b85cc4ef53ed69bbe1
Philic <xlw125414713@163.com>
2017-03-27 19:54:07 +0800

    ajax post init

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,9 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { TabsPage } from '../tabs/tabs';
 
+import { Http, Headers } from '@angular/http';
+import 'rxjs/Rx';
+
 /*
   Generated class for the Login page.
 
@@ -15,7 +18,12 @@ import { TabsPage } from '../tabs/tabs';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http:Http) {}
+
+  userInfo = {
+      Account: '',
+      Password: ''
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
@@ -26,4 +34,19 @@ export class LoginPage {
       this.navCtrl.push(TabsPage);
   }
 
+  login() {
+      //this.toMainPage();
+    //   let headers = new Headers();
+    //   headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    //   let options = new RequestOptions({ headers: headers });
+      let url = 'http://192.168.1.59:3000';
+      let uri = '/authority/getAllUsers';
+      let body = {};
+      console.log(this.userInfo.Account);
+      this.http.post(url+uri, body)
+        .map(res => res.json())
+        .subscribe(data => {
+            console.log(data);
+        });
+  }
 }
